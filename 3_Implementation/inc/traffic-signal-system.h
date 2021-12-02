9eeae4dac838153203092e8374a624a80b011001
SATHIZ13 <89175883+SATHIZ13@users.noreply.github.com>
2021-12-02 13:43:07 +0530

    Update traffic-signal-system.h

--- 3_Implementation/inc/traffic-signal-system.h
+++ 3_Implementation/inc/traffic-signal-system.h
@@ -1,3 +1,6 @@
+#ifndef _TRAFFIC_SIGNAL_SYSTEM_H__
+#define __TRAFFIC-SIGNAL-SYSTEM_H__
+
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -11,5 +14,11 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
 
-#endif /* __TRAFFIC_SIGNAL_SYSTEM_H__ */
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB1)   /**< LED Pin number  */
+
+#include <avr/io.h>
+
+#endif /* _TRAFFIC_SIGNAL_SYSTEM_H__ */
