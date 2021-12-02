f0502ab826451e2a5c12be58bee90e9db4cb9bde
SATHIZ13 <89175883+SATHIZ13@users.noreply.github.com>
2021-12-02 13:33:27 +0530

    Update traffic-signal-system.h

--- 3_Implementation/inc/traffic-signal-system.h
+++ 3_Implementation/inc/traffic-signal-system.h
@@ -1,1 +1,15 @@
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+/**
+ * @file traffic-signal-system.h
+ * @author SATHIZ13 ()
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-012-1
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#endif /* __TRAFFIC_SIGNAL_SYSTEM_H__ */
